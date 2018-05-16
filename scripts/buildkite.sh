75b04759fdf6bcab837ad19ed31b6ecc36ee6cc0
Robert Matesic <rmatesic@users.noreply.github.com>
2018-05-16 14:00:45 +1000

    Create buildkite.sh

---
+++ scripts/buildkite.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source "$HOME/.bashrc"
+
+echo '--- bundling'
+bundle install
+npm install
+
+echo '--- preparing database'
+./bin/rake db:create RAILS_ENV=test
+./bin/rake db:test:prepare
+
+echo '--- running specs'
+./bin/rspec
+
+echo '--- upload coverage results'
+buildkite-agent artifact upload "coverage/.resultset.json"
